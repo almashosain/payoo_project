7a02f15c4414092b4ee2e311a73d8f0f14114958
Almas Hosain Rifat <almashossainrifat@gmail.com>
2025-08-26 04:05:23 +0600

    added toggling

--- home.js
+++ home.js
@@ -1,33 +1,98 @@
 const validPin = 1234
 
-// Add Money Feature 
+// Function to get input values 
+
+function getInputValueNumber (id) {
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue)
+    
+    return inputFieldValueNumber
+}
+
+function getInputValue (id) {
+      const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    return inputFieldValue
+}
+
+// Function to get inner text
+
+function getInnerText (id){
+    const element = document.getElementById(id)
+    const elementValue = element.innerText
+    const elementValueNumber = parseInt(elementValue)
+return elementValueNumber
+}
+
+// Function to set innertext
+
+function setInnerText (value) {
+    const availableBalanceElement = document.
+    getElementById('available_balance')
+    availableBalanceElement.innerText = value
+}
+
+// Function to toggle
+function handleToggle(id){
+    const forms  = document.getElementsByClassName('form')
+
+for(const form of forms){
+   form.style.display = 'none'
+}
+document.getElementById(id).style.display = 'block'
+}
+
+// Function To Toggle Buttons
+
+function handleButtonToggle(id){
+    const formBtns = document.getElementsByClassName
+    ('form_btn')
+    
+    for(const btn of formBtns){
+       btn.classList.remove("border-[#0874f2]","bg-[#0874f20d]")
+       btn.classList.add("border-gray-300")
+
+    }
+
+ document.getElementById(id).classList.remove
+        ("border-gray-300")
+ document.getElementById(id).classList.add
+        ("border-[#0874f2]","bg-[#0874f20d]")
+      
+}
+
+
+// Add Money Feature
+
 document.getElementById('add_money_btn')
 .addEventListener('click', function(e){
     e.preventDefault()
-    const bank = document.getElementById('bank').value
-    const accountNumber = document.getElementById('account_number')
-    .value
+    const bank = getInputValue('bank')
+    const accountNumber = document.getElementById('account_number').value
+    
+    const amount = getInputValueNumber('add_amount')
+    const pinNumber = getInputValueNumber('add_pin')
 
-    const amount = parseInt(document.getElementById('add_amount').value)
-    const pin =parseInt(document.getElementById('add_pin').value)
+    const availableBalance = getInnerText ('available_balance')
+ 
+    
+if(accountNumber.length < 11){
+    alert('Invalid account Number')
+    return;
+}
 
-    const availableBalance = parseInt(document.getElementById('available_balance').innerText )
+if (pinNumber !== validPin){
+    alert('Invalid Pin Number')
+    return;
+}
 
-    if(accountNumber.length <11){
-        alert('Please provide valid account number')
-        return;
-    }
+ const totalNewAvailableBalance = amount + availableBalance
 
-    if(pin !== validPin){
-        alert('Please provide valid pin number')
-        return;
-    }
-
-    const totalNewAvailableBalance = amount + availableBalance
-    document.getElementById('available_balance').innerText = totalNewAvailableBalance
-
-
+ document.getElementById('available_balance').innerText = totalNewAvailableBalance
 })
+
+
 
 // CashOut Money Feature 
 
@@ -35,35 +100,107 @@ document.getElementById('withdraw_btn')
 .addEventListener('click', function(e){
     e.preventDefault()
    
-    const amount = parseInt(document.getElementById('withdraw_amount').value)
+    const amount = getInputValueNumber('withdraw_amount')
     
-    const availableBalance =parseInt( document.getElementById('available_balance').innerText)
+    const availableBalance = getInnerText('available_balance')
     
     const totalNewAvailableBalance = availableBalance - amount
 
-    document.getElementById('available_balance').innerText = totalNewAvailableBalance
+    setInnerText(totalNewAvailableBalance)
 })
-
-
-
-
-
-
-
 
 
 
 // Toggling Feature
 
-document.getElementById('add_money_button').addEventListener('click',function(){
-    document.getElementById('cash_out_parent').style.display = 'none'
+document.getElementById('add_money_button').addEventListener('click',function(e){
 
-    document.getElementById('add_money_parent').style.display = 'block'
+handleToggle('add_money_parent')
+
+handleButtonToggle('add_money_button')   
+      
+
 })
+//  
 document.getElementById('cash_out_button').addEventListener('click',function(){
-      document.getElementById('add_money_parent').style.display = 'none'
+handleToggle('cash_out_parent')
+handleButtonToggle('cash_out_button')
+})
 
-    document.getElementById('cash_out_parent').style.display = 'block'
+document.getElementById('transfer_btn')
+.addEventListener('click', function(){
+handleToggle('transfer_money_parent')
+
+handleButtonToggle('transfer_btn')
+})
+
+document.getElementById('bonus_btn')
+.addEventListener('click', function(){
+
+  handleToggle('get_bonus_parent')
+
+  handleButtonToggle('bonus_btn')
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('add_money_button').addEventListener('click',function(){
+//     document.getElementById('cash_out_parent').style.display = 'none'
+
+//     document.getElementById('add_money_parent').style.display = 'block'
+// })
+// document.getElementById('cash_out_button').addEventListener('click',function(){
+//       document.getElementById('add_money_parent').style.display = 'none'
+
+//     document.getElementById('cash_out_parent').style.display = 'block'
+// })
+
+
+// -------------------------------------
+
+
+// document.getElementById('add_money_btn')
+// .addEventListener('click', function(e){
+//     e.preventDefault()
+//     const bank = document.getElementById('bank').value
+//     const accountNumber = document.getElementById('account_number')
+//     .value
+
+//     const amount = parseInt(document.getElementById('add_amount').value)
+//     const pin =parseInt(document.getElementById('add_pin').value)
+
+//     const availableBalance = parseInt(document.getElementById('available_balance').innerText )
+
+//     if(accountNumber.length <11){
+//         alert('Please provide valid account number')
+//         return;
+//     }
+
+//     if(pin !== validPin){
+//         alert('Please provide valid pin number')
+//         return;
+//     }
+
+//     const totalNewAvailableBalance = amount + availableBalance
+//     document.getElementById('available_balance').innerText = totalNewAvailableBalance
+
+
+// })
--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 // Login Button Functionality 
 
 document.getElementById('loginButton')
-.addEventListener('click' , function(e){
+.addEventListener('click',function(e){
     e.preventDefault()
     const mobileNumber = 12345678910
-    const pinNumber = 1234
-    const mobileNumberValue = document.getElementById('mobile_number').value
+    const pin = 1234
+
+    const mobileNumberValue = document.
+    getElementById('mobile_number').value
     const mobileNumberValueConverted = parseInt(mobileNumberValue)
-    const pinNumberValue = document.getElementById('pin_number').value
-    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    const pinNumber = document.getElementById('pin_number').value
+    const pinNumberValueConverted = parseInt(pinNumber)
+
+    if(mobileNumberValueConverted === mobileNumber &&
+        pinNumberValueConverted === pin ){
+            window.location.href="./home.html"
+        }
+        else{
+            alert('Invalid Credentials')
+        }
+})
 
 
-    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
-        window.location.href="./home.html"
-    }
-    else{
-        alert("Invalid Credentials")
-    }
-} )
+
+// .addEventListener('click' , function(e){
+//     e.preventDefault()
+//     const mobileNumber = 12345678910
+//     const pinNumber = 1234
+//     const mobileNumberValue = document.getElementById('mobile_number').value
+//     const mobileNumberValueConverted = parseInt(mobileNumberValue)
+//     const pinNumberValue = document.getElementById('pin_number').value
+//     const pinNumberValueConverted = parseInt(pinNumberValue)
+
+
+//     if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+//         window.location.href="./home.html"
+//     }
+//     else{
+//         alert("Invalid Credentials")
+//     }
+// } )
